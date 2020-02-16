4cad65b010201b6f961520fab6df6171ed12916a
effus <effusps@gmail.com>
2020-02-16 23:10:42 +0300

    json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const mongoose = require('./mongo');
 
 const Api100 = require('./app/api.1.0.0');
 
