9648b157a1814d1a7effea9b0c247f83f1b1441e
effus <effusps@gmail.com>
2020-02-16 23:12:57 +0300

    json

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env = process.env.NODE_ENV;
-const config = require('mongo.json')[env];
+const config = require('./mongo.json')[env];
 
 module.exports = () => {
     const envUrl = process.env[config.use_env_variable];
