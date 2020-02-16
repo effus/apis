222572c503e70b0e884c92a1c32a190cd4de5d5a
effus <effusps@gmail.com>
2020-02-17 00:08:03 +0300

    json

--- app/api.1.0.0.js
+++ app/api.1.0.0.js
@@ -13,19 +13,21 @@ class Api100 {
      * @param {*} res 
      */
     register(req, res) {
-        this.res = res;
-        this.Db.connect().then((connect) => {
-            const users = connect.collection('users');
-            users.find().toArray((err, results) => {
-                res.send({
-                    users: results 
+        try {
+            this.Db.connect().then((connect) => {
+                connect.collection('users').find().toArray((err, results) => {
+                    res.send({
+                        users: results 
+                    });
                 });
-            });
-        }).catch(this.error);
+            }).catch((e) => this.error(res, e));
+        } catch (e) {
+            this.error(res, e);
+        }
     }
 
-    error(exception) {
-        this.res.send({error:true, message: exception.message});
+    error(res, exception) {
+        res.send({error:true, message: exception.message});
     }
 };
 
