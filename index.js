c1fc5c1f76534727a7af48a75b7893c2e760bc2d
effus <effusps@gmail.com>
2020-02-16 19:59:32 +0300

    json

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/meetapi', (req, res) => {
-    return {
-        'api': {
-            'title': 'meet',
-            'vers': '1.0.0'
-        }
-    };
-})
+    res.json({foo: 'bar'});
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
