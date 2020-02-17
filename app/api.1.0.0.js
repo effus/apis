afc7d5d18e062f8f92c69507b70cf39ad424b27e
effus <effusps@gmail.com>
2020-02-17 21:16:40 +0300

    up

--- app/api.1.0.0.js
+++ app/api.1.0.0.js
@@ -7,35 +7,20 @@ class Api100 {
     
     getCollection(name) {
         return new Promise((resolve, reject) => {
-            this.Db.connect().then((connect) => {
-                resolve(connect.collection(name));
+            this.Db.connect().then((client) => {
+                const db = client.db('heroku_xjdq05dr');
+                resolve(db.collection(name));
             }).catch(reject);
         });
     }
 
     register(req, res) {
-        // getCollection('users').then((collection) => {
-        //     res.send({
-        //         users: results 
-        //     });
-        // }).catch((e) => {
-        //     console.log('Api1.0.0:register error', e);
-        //     res.send({error:true});
-        // });
-            // .find().toArray((err, results) => {
-                
-            // });
-        //})
-        Db.connect().then((client) => {
-            //const users = connect.collection('users');
-            const db = client.db('heroku_xjdq05dr');
-            const users = db.collection('users').find().toArray((err, items) => {
-                console.log('register', items);
-                res.send({wtf:true, items: items});
-            });
-        }).catch((e) => {
-            console.debug('error', e);
+        
+        this.getCollection('users').find().toArray((err, items) => {
+            console.log('register', items);
+            res.send({wtf:true, items: items});
         });
+    
         
     }
 
