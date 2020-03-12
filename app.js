71da5ccd09b2cdc8fc7f507d9b072027e4cb9b28
evgeny.konoplev <evgeny.konoplev@gemotest.ru>
2020-03-12 11:16:20 +0300

    new express config

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require('express')
+const express = require('express');
+const PORT = process.env.PORT || 5000;
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
-
-
 const Api100 = require('./app/api.1.0.0');
-
 let app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/ping', (req, res) => {
+    res.send({result: true});
+});
 app.get('/apiinfo', (req, res) => {
   Db.connect().then(() => {
     res.send({ apis: ['1.0.0'], db: {connected: true}});
